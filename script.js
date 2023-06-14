7105eaf4b5a833a421c997c3e501f26a7c6ee52e
phoenixparisian <phoenixparisian@gmail.com>
2023-06-13 21:57:59 -0700

    initial commit, added HTML and script

---
+++ script.js
@@ -0,0 +1,117 @@
+const selections = ["Rock", "Paper", "Scissors"];
+
+// DOM elements
+const main = document.querySelector("main");
+const buttons = document.querySelectorAll(".choice-btn");
+const roundNumText = document.querySelector("#round-num");
+const playerChoiceText = document.querySelector("#player-choice");
+const computerChoiceText = document.querySelector("#computer-choice");
+const roundOutcomeText = document.querySelector("#round-outcome");
+const totalText = document.querySelector("#total-score");
+
+
+const winHTML = "<p> You Won! <p>"
+const lossHTML = "<p> Game over. You lost.<p>"
+
+let roundCount = 0;
+let playerScore = 0;
+let computerScore = 0;
+
+let computerSelection;
+let playerSelection;
+
+
+function getComputerChoice() {
+  const randomChoice = selections[Math.floor(Math.random() * selections.length)];
+  return randomChoice;
+}
+
+function playRound(playerSelection, computerSelection) {
+  let outcome;
+
+  if (playerSelection == "Rock") {
+    if (computerSelection == "Rock") {
+      outcome = "Draw";
+    } else if (computerSelection == "Paper") {
+      outcome = "Loss";
+    } else { 
+      outcome = "Win";
+    }
+  }
+
+  if (playerSelection == "Paper") {
+    if (computerSelection == "Rock") {
+      outcome = "Win";
+    } else if (computerSelection == "Paper") {
+      outcome = "Draw";
+    } else { 
+      outcome = "Loss";
+    }
+  }
+
+  if (playerSelection == "Scissors") {
+    if (computerSelection == "Rock") {
+      outcome = "Loss";
+    } else if (computerSelection == "Paper") {
+      outcome = "Win";
+    } else { 
+      outcome = "Draw";
+    }
+  }
+  if (outcome == "Win") {
+    playerScore ++;
+  } else if (outcome == "Loss") {
+    computerScore ++; 
+  }
+
+  if (playerScore > 4) {
+    main.innerHTML = winHTML;
+  }
+
+  if (computerScore > 4) {
+    main.innerHTML = lossHTML;
+  }
+
+
+  roundOutcomeText.textContent = "Outcome: " + outcome;
+
+  totalText.textContent = "Player: " + playerScore + " Computer: " + computerScore;
+
+
+}
+
+
+function playGame(event) {
+  roundCount++;
+  roundNumText.textContent = "Round " + roundCount;
+
+  const btnId = event.target.id;
+
+  if (btnId == "rock-btn") {
+    playerSelection = "Rock";
+  }
+  if (btnId == "paper-btn") {
+    playerSelection = "Paper";
+  }
+  if (btnId == "scissors-btn") {
+    playerSelection = "Scissors";
+  }
+
+  computerSelection = getComputerChoice();
+
+  playerChoiceText.textContent = "Your Selection: " + playerSelection;
+  computerChoiceText.textContent = "Computer's Selection: " + computerSelection;
+
+
+
+  playRound(playerSelection, computerSelection);
+
+
+
+
+}
+
+
+buttons.forEach((button) => {
+  button.addEventListener("click", playGame)
+});
